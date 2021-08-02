40d9fcdc1bd9819e74136baa0e05cf334dae0052
João Vítor Carvalho <mistingerr@gmail.com>
2021-08-02 13:26:36 -0300

    refactored modal and fixed bugs

--- animais-fantasticos/js/modules/modal.js
+++ animais-fantasticos/js/modules/modal.js
@@ -1,21 +1,44 @@
-export default function initModal() {
-  const btnAbrir = document.querySelector('[data-modal="abrir"]');
-  const btnFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(btnOpen, btnClose, containerModal) {
+    this.btnOpen = document.querySelector(btnOpen);
+    this.btnClose = document.querySelector(btnClose);
+    this.containerModal = document.querySelector(containerModal);
 
-  function toggleModal(event) {
+    // bind this to the callback doing
+    // reference to the object class
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clickOutModal = this.clickOutModal.bind(this);
+  }
+
+  // open and close modal
+  toggleModal() {
+    this.containerModal.classList.toggle('ativo');
+  }
+
+  // add toggle to modal
+  eventToggleModal(event) {
     event.preventDefault();
-    containerModal.classList.toggle('ativo');
+    this.toggleModal();
   }
 
-  function clickOutModal(event) {
-    if (event.target === this);
-    toggleModal(event);
+  // close modal when clicking outside the container
+  clickOutModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal(event);
+    }
   }
 
-  if (btnAbrir && btnFechar && containerModal) {
-    btnAbrir.addEventListener('click', toggleModal);
-    btnFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', clickOutModal);
+  // add events to the modal elements
+  addModalEvents() {
+    this.btnOpen.addEventListener('click', this.eventToggleModal);
+    this.btnClose.addEventListener('click', this.eventToggleModal);
+    this.containerModal.addEventListener('click', this.clickOutModal);
+  }
+
+  init() {
+    if (this.btnOpen && this.btnClose && this.containerModal) {
+      this.addModalEvents();
+    }
+    return this;
   }
 }
